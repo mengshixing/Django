53de287f24310431a00ef1204cbb616aa928789f
mengshixing <shixingmeng@gmail.com>
2017-06-11 21:08:29 +0800

    reduce complate

--- python3 note/chapter 4 函数式编程/function.py
+++ python3 note/chapter 4 函数式编程/function.py
@@ -37,3 +37,90 @@ def f1(x):
 print(list(map(f1,range(1,6)))) 
 
 print(list(map(str,'hello world !')))
+
+#reduce函数等于函数执行后的值作为下一层嵌套的值
+
+#reduce(f,[1,2,3,4])=f(f(f(1,2),3),4)
+
+def f2(x,y):
+
+    return x+y
+
+from functools import reduce
+    
+print(reduce(f2,range(1,6)))
+
+#字符串转换为整数
+
+def f3(x,y):
+
+    return x*10+y
+    
+def char2num(s):
+
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+    
+print(reduce(f3,map(char2num,'12345')))
+
+#优化成一个函数
+
+def str2int(s):
+    
+    def f4(x,y):
+
+        return x*10+y
+        
+    def char2num(s):
+
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+        
+    return reduce(f4,map(char2num,s))
+    
+print(str2int('1234589'))
+
+#使用lambda函数进一步优化(匿名函数)
+        
+def str2int(s):
+    
+    return reduce(lambda x,y:x*10+y,map(char2num,s))
+    
+print(str2int('87892'))
+
+#练习1,输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+
+list1=['adam', 'LISA', 'barT']
+
+print(list(map(lambda x:x.title(),list1)))
+
+#练习2,请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+
+def prod(l):
+
+    return reduce(lambda x,y:x*y,l)
+
+print(prod(range(1,6)))
+
+#练习3,利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+
+def str2float(s):
+
+    return reduce(lambda x,y:x*10+y,map(char2num,s.split('.')[0]+s.split('.')[1]))*(10**-len(s.split('.')[1]))
+
+print(str2float('123.456'))
+
+#filter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

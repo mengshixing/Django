32d368256add0b5292570480000842faab10566e
mengshixing <shixingmeng@gmail.com>
2017-06-16 18:37:04 +0800

    higobj

--- python3 note/chapter 7 面向对象高级编程/highobj.py
+++ python3 note/chapter 7 面向对象高级编程/highobj.py
@@ -128,5 +128,70 @@ p=Parrot()
 p.do()
 p.run()
 
-#关于定制类
+#关于定制类,__slots__能限制实例能添加的属性,
+#__xxx__这种格式的变量和函数一般有特殊意义
+
+class Personal():
+    def __init__(self,name):
+        self.__name=name
+print(Personal('xiaoy'))
+
+#__str__使用优化输出,__repr__用于调试显示
+class Personal1():
+    def __init__(self,name):
+        self.__name=name
+    def __str__(self):
+        return "Personal1 object :(name : %s)" % self.__name
+    __repr__=__str__
+print(Personal1('xiaoy'))
+Personal1('xiaoyy')
+
+#__iter__使一个类可以for in 循环,__next__调用
+
+class Fibseries():
+    def __init__(self):
+        self.a,self.b=1,2
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.a,self.b=self.b,self.a+self.b
+        if self.a>100:
+            raise StopIteration()
+        return self.a
+    
+    #此时类虽然可以for循环,但是不能当list使用,需要__getitem__()方法  
+	#__getitem__()方法传入参数可能有多种
+    def __getitem__(self,n):
+        a,b=1,1
+        for x in range(n):
+            a,b=b,a+b
+        return a
+    
+        
+for n in Fibseries():
+    print(n)
+
+print(Fibseries()[3])
+print(Fibseries()[13])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

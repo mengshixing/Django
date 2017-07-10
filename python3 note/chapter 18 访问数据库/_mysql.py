01762d9ad9a037a01bd79beda56b4a7a0052b925
mengshixing <shixingmeng@gmail.com>
2017-07-10 18:33:16 +0800

    mysql pymysql

--- python3 note/chapter 18 访问数据库/_mysql.py
+++ python3 note/chapter 18 访问数据库/_mysql.py
@@ -15,12 +15,12 @@
 
     # chkconfig --list | grep mysqld命令来查看mysql服务是不是开机自动启动
     # chkconfig mysqld on 命令来将其设置成开机启动
-	# 1/etc/my.cnf 这是mysql的主配置文件
-	# 2./var/lib/mysql   mysql数据库的数据库文件存放位置
-	# 3./var/log mysql数据库的日志输出存放位置
-	
-	
-	
+    # 1/etc/my.cnf 这是mysql的主配置文件
+    # 2./var/lib/mysql   mysql数据库的数据库文件存放位置
+    # 3./var/log mysql数据库的日志输出存放位置
+    
+    
+    
 # 由于MySQL服务器以独立的进程运行，并通过网络对外服务，所以，需要支持Python的MySQL驱动
 # 来连接到MySQL服务器。MySQL官方提供了mysql-connector-python驱动，
 # 但是安装的时候需要给pip命令加上参数--allow-external：
@@ -29,11 +29,35 @@
 # 如果上面的命令安装失败，可以试试另一个驱动：
 # $ pip install mysql-connector
 
+#都不行的话换一个库pip install pymysql或者pip3 install pymysql
+
 #在window10 64测试  pip install mysql-connector可以正常安装
 
-import mysql.connector
+#import mysql.connector
+import pymysql
 
-conn=mysql.connector.connect(user='root',password='root',database='test')
+conn=pymysql.Connect(user='root',password='123456',database='test',charset='utf8')
 
 cursor=conn.cursor()
+
+cursor.execute('create table user (id varchar(25) primary key,name varchar(25))')
+
+cursor.execute('insert into user (id,name) values (\'2\',\'tom\')')
+print(cursor.rowcount)
+
+#执行INSERT等操作后要调用commit()提交事务
+conn.commit()#提交
+
+cursor.close()#关闭
+
+cursor=conn.cursor()
+
+#MySQL的SQL占位符是%s。
+cursor.execute('select * from user where id = %s',('2',))
+
+print(cursor.fetchall())
+
+cursor.close()
+
+conn.close()
 
